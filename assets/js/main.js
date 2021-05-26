a1f5fdcf57b20fad709c4fe97c9e767ba0027a33
himanshu <hoberoi@taksatech.com>
2021-05-26 06:14:47 +0530

    stripe validation

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,21 @@ $(document).ready(function(){
   $('.amountButton').click(function(){
     $('#customAmount').val($(this).data('value'))
   })
+
+  //Stripe Related Code
+  let stripe = Stripe('pk_test_PWq57B1bp5fKRpPCflyvASE2')
+  let elements = stripe.elements()
+  let cardElement = elements.create('card',{
+    hidePostalCode: true
+  });
+  cardElement.mount('#card-element')
+  cardElement.on('change', function(event) {
+    if (event.complete) {
+      $('#donateButton').prop('disabled',false)
+    } else if (event.error) {
+      console.log('failed')
+    }
+  });
 })
 
 // Address Auto-Complete
@@ -18,7 +33,7 @@ function initAutocomplete() {
   // Create the autocomplete object, restricting the search predictions to
   // addresses in the US and Canada.
   autocomplete = new google.maps.places.Autocomplete(address1Field, {
-    componentRestrictions: { country: ["us", "ca"] },
+    componentRestrictions: { country: ["us", "ca", "in"] },
     fields: ["address_components", "geometry"],
     types: ["address"],
   });
@@ -34,7 +49,10 @@ function fillInAddress() {
   let postcode = "";
   for (const component of place.address_components) {
     const componentType = component.types[0];
-
+    console.log('#######')
+    console.log(componentType)
+    console.log(component.long_name)
+    console.log('#######')
     switch (componentType) {
       case "street_number": {
         address1 = `${component.long_name} ${address1}`;
@@ -48,6 +66,7 @@ function fillInAddress() {
 
       case "postal_code": {
         postcode = `${component.long_name}${postcode}`;
+        document.querySelector("#zipCode").value = postcode
         break;
       }
 
@@ -56,16 +75,17 @@ function fillInAddress() {
         break;
       }
       case "locality":
-        document.querySelector("#locality").value = component.long_name;
+        document.querySelector("#streetAddress").value = component.long_name;
         break;
 
       case "administrative_area_level_1": {
         document.querySelector("#state").value = component.short_name;
         break;
       }
-      case "country":
-        document.querySelector("#country").value = component.long_name;
+      case "administrative_area_level_2": {
+        document.querySelector("#city").value = component.short_name;
         break;
+      }
     }
   }
   address1Field.value = address1;
